c76606970ac81af555073b9d370e87311994e195
natautomation <147779652+natautomation@users.noreply.github.com>
2023-12-02 04:53:14 -0800

    Create 4_update_repo.py

---
+++ requests/4_update_repo.py
@@ -0,0 +1,31 @@
+"""Send PATCH request to update"""
+
+import requests
+
+def update_repo(url_):
+    """PATCH function"""
+    # adding code here
+  
+    header_content = {'Authorization': 'token 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'}
+
+    payload = {'description': 'I know Python Requests!'}
+
+    response = requests.patch(url_, headers=header_content
+                              , json=payload)
+
+    print(f'Response status code: {response.status_code}')
+    repo_ = response.json()
+
+    return repo_
+
+
+if __name__ == '__main__':
+    owner = 'natautomation'
+    repo = 'repo-created-with-api'
+    url = f'https://api.github.com/repos/{owner}/{repo}'
+
+    repo = update_repo(url)
+
+
+#output
+#Process finished with exit code 0
