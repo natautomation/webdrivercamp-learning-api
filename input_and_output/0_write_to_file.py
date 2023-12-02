f695f01eb199b09eb8905ccae6825ca7af4dd71e
natautomation <147779652+natautomation@users.noreply.github.com>
2023-12-01 18:40:59 -0800

    Update 0_write_to_file.py

--- input_and_output/0_write_to_file.py
+++ input_and_output/0_write_to_file.py
@@ -35,3 +35,29 @@ if __name__ == "__main__":
     data = "Don't Panic"
     print(f"Number of char: {write_to_file(data, file_name)}")
     print(f"Files: {next(os.walk(dir_path), (None, None, []))[2]}")
+    
+#output
+# Files: ['3_to_json.py', '1_append_to_file_0.py', '5_from_json_file.py', '4_json_to_file_0.py',
+    #         '0_write_to_file_0.py', '1_append_to_file.py', '2_read_a_file_0.py', 'data.json', 'README.md',
+    #         '3_to_json_0.py', '5_from_json_file_0.py', '0_write_to_file.py', '4_json_to_file.py',
+    #         'my_info_token_natautomation.txt', 'data.txt', '2_read_a_file.py']
+    #
+    # Number
+    # of
+    # char: 133
+    # Files: ['3_to_json.py', '1_append_to_file_0.py', '5_from_json_file.py', '4_json_to_file_0.py',
+    #         '0_write_to_file_0.py', '1_append_to_file.py', '2_read_a_file_0.py', 'data.json', 'README.md',
+    #         '3_to_json_0.py', '5_from_json_file_0.py', '0_write_to_file.py', '4_json_to_file.py',
+    #         'my_info_token_natautomation.txt', 'data.txt', '2_read_a_file.py']
+    #
+    # Number
+    # of
+    # char: 11
+    # Files: ['3_to_json.py', '1_append_to_file_0.py', '5_from_json_file.py', '4_json_to_file_0.py',
+    #         '0_write_to_file_0.py', '1_append_to_file.py', '2_read_a_file_0.py', 'data.json', 'README.md',
+    #         '3_to_json_0.py', '5_from_json_file_0.py', '0_write_to_file.py', '4_json_to_file.py',
+    #         'my_info_token_natautomation.txt', 'data.txt', '2_read_a_file.py']
+    #
+    # Process
+    # finished
+    # with exit code 0
