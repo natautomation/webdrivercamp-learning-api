19f1cde6c29e0736eb3ceb9729c981ccd0e42a88
natautomation <147779652+natautomation@users.noreply.github.com>
2023-12-01 21:32:22 -0800

    Create 3_get_created_repo.py

---
+++ requests/3_get_created_repo.py
@@ -0,0 +1,27 @@
+
+"""GET the newly created repo"""
+
+import requests
+
+def get_created_repo(url_):
+    """GET repo and assert values"""
+    # adding code
+    
+    header_content = {'Authorization': 'token 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'}
+    response = requests.get(url_, headers=header_content)
+
+    print(f'Response status code: {response.status_code}')
+    repo = response.json()
+    #- 
+    assert not repo["has_wiki"] != False
+    assert repo['private'] == True
+    assert repo['name'] == 'repo-created-with-api'
+    assert repo['owner']['login'] == 'natautomation'
+
+if __name__ == "__main__":
+    v_data = {"owner": {"login": "natautomation", "id": 147779652}, "name": "repo-created-with-api",
+              "id": 725440644, "has_wiki": False, "private": True}
+    owner = 'natautomation'
+    repo = 'repo-created-with-api'
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+    get_created_repo(url)
