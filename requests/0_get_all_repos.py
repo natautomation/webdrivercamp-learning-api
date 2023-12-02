72a085a636f2af988e3cebabbe219b524dbd713f
natautomation <147779652+natautomation@users.noreply.github.com>
2023-12-01 19:05:21 -0800

    Update 0_get_all_repos.py

--- requests/0_get_all_repos.py
+++ requests/0_get_all_repos.py
@@ -29,29 +29,5 @@ if __name__ == "__main__":
 # Response status code: 200
 # Total count of found items: 24
 #
-# Acteonis        webdrivercamp-learning-python-2
-# Acteonis        webdrivercamp-learning-python
-# AlinaKaty       webdrivercamp-learning-python
-# IgorSuvorov     webdrivercamp-learning-python
-# JuliaGood       webdrivercamp-learning-python
-# PieVan          webdrivercamp-learning-python
-# bakhtier23      webdrivercamp-learning-python-python_beginning
-# bakhtier23      webdrivercamp-learning-python-python-selenium
-# bakhtier23      webdrivercamp-learning-python-python-modules
-# bakhtier23      webdrivercamp-learning-python-python-data-structures-continue
-# bakhtier23      webdrivercamp-learning-python-python-loops-and-more
-# bakhtier23      webdrivercamp-learning-python-python-errors-exceptions
-# chekirovstas    webdrivercamp-learning-python
-# dvlad7909       webdrivercamp-learning-python
-# ehnat0n         webdrivercamp-learning-python
-# eli-atlanta     webdrivercamp-learning-python
-# emnyc1221       webdrivercamp-learning-python
-# galiur          webdrivercamp-learning-python
-# isimionica      webdrivercamp-learning-python
-# natautomation   webdrivercamp-learning-python
-# oleksrud        webdrivercamp-learning-python
-# shaikin-s       webdrivercamp-learning-python
-# silvervinoth9   webdrivercamp-learning-python
-# vr8686          webdrivercamp-learning-python
-#
+# --named all  -- ...
 # Process finished with exit code 0
